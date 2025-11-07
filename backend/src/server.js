fe8de336b5b8406edd0ee21107a9d25276517f2d
Anshuman Sinha <anshuman@engineer.com>
2025-11-07 20:10:50 +0530

    minor correction

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV !== "production") {
 	app.use(cors());
 }
 app.use("/api", routes);
